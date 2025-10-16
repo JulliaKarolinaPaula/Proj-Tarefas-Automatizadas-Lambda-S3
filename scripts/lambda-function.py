0bd484ded37be1d03ca1a7b6c0298969b5a17a10
Jullia Karolina <135389065+JulliaKarolinaPaula@users.noreply.github.com>
2025-10-16 16:02:42 -0300

    Update lambda-function.py

--- scripts/lambda-function.py
+++ scripts/lambda-function.py
@@ -5,9 +5,9 @@ def lambda_handler(event, context):
     print("Evento recebido:", json.dumps(event))
     
     # Exemplo de manipulação de objeto
-    # Aqui você pode colocar lógica para alterar, filtrar ou transformar o conteúdo
     
     return {
         'statusCode': 200,
         'body': json.dumps('Lambda executada com sucesso!')
     }
+
